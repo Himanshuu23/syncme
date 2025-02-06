48aabd425be1f534f806ed94dd64407061196dab
unnat7779 <agrawalunnat7779@gmial.com>
2025-02-06 18:50:59 +0530

    fixed bug

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import "./globals.css";
 
 import { useState } from "react";
@@ -12,19 +12,15 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <div className="relative min-h-screen bg-slate-950 overflow-hidden">
-          <div className="z-0 absolute bottom-0 left-[-10%] right-0 top-[-10%] size-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]" />
+          <div className="-z-0 absolute bottom-0 left-[-10%] right-0 top-[-10%] size-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]" />
           <div className="z-0 absolute bottom-0 right-[-10%] top-[-10%] size-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]" />
-          <Navbar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
-          <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
-          <main
-            className={`
-        pt-16
-        ${isSidebarOpen ? "lg:ml-60" : "ml-0"}
-        transition-all duration-300 ease-in-out
-      `}
-          ></main>
-
-          {children}
+          <Navbar
+            isSidebarOpen={isSidebarOpen}
+            setIsSidebarOpen={setIsSidebarOpen}
+          />
+          <div className={`flex ${isSidebarOpen ? 'ml-64' : 'w-full'} transition-all`}>
+            {children}
+          </div>
         </div>
       </body>
     </html>
